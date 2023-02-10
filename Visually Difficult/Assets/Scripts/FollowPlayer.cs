3249e52f2c070c35c46f5a4d662056df6fc59c86
BigUngaBunga <truls.rockstrom.ystad.nu@gmail.com>
2023-02-10 15:10:45 +0100

    Improved camera and movement
    Made the camera smooth and improved jumping, moving and made falling out of the world restart the level

---
+++ Visually Difficult/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    [SerializeField] private GameObject target;
+    [Range(1f, 10f)]
+    [SerializeField] private float interpolationSpeed;
+    [SerializeField] private Vector3 offset;
+
+    private void FixedUpdate()
+    {
+        Vector3 targetPosition = target.transform.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetPosition, interpolationSpeed * Time.deltaTime);
+    }
+}
--- Visually Difficult/Assets/Scripts/PlayerControler.cs
+++ Visually Difficult/Assets/Scripts/PlayerControler.cs
@@ -5,12 +5,18 @@ using UnityEngine.InputSystem;
 
 public class PlayerControler : MonoBehaviour
 {
-    private PlayerInput input;
-    private Rigidbody2D rigidbody;
     [SerializeField] private float speed;
     [SerializeField] private float jumpForce;
-    private float forceConstant = 100f;
+    [SerializeField] private LayerMask groundMask;
 
+    [SerializeField] private Vector2 groundCheckOffset;
+    [SerializeField] private Vector2 groundCheckSize;
+    private Vector2 GroundCheckPosition => (Vector2)transform.position + groundCheckOffset;
+
+
+    private PlayerInput input;
+    private Rigidbody2D rigidbody;
+    private float forceConstant = 100f;
     #region Actions
 
     private InputAction moveAction;
@@ -37,15 +43,28 @@ public class PlayerControler : MonoBehaviour
 
     private void Jump(InputAction.CallbackContext context)
     {
-        Vector2 force = Vector2.up * jumpForce * Time.deltaTime * forceConstant;
+        if (!Physics2D.OverlapBox(GroundCheckPosition, groundCheckSize, 0f, groundMask)) return;
+        Vector2 force = Vector2.up * jumpForce;
+        ResetY();
         rigidbody.AddForce(force, ForceMode2D.Impulse);
         Debug.Log("Jumped with force: " + force);
     }
 
     private void AddMovement(Vector2 direction)
     {
-        Vector2 force = direction * speed * Time.deltaTime * forceConstant;
-        rigidbody.AddForce(force);
-        if (direction != Vector2.zero) { Debug.Log("Added force: " + force); }
+        //TODO använd lerp för mjukare rörese
+        ResetX();
+        Vector2 speed = direction * this.speed * Time.deltaTime * forceConstant;
+        rigidbody.velocity = new Vector2(speed.x, rigidbody.velocity.y);
+        if (direction != Vector2.zero) { Debug.Log("Set speed: " + speed); }
+    }
+
+    private void ResetX() => rigidbody.velocity.Set(0, rigidbody.velocity.y);
+    private void ResetY() => rigidbody.velocity.Set(rigidbody.velocity.x, 0);
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawCube(GroundCheckPosition, groundCheckSize);
     }
 }
