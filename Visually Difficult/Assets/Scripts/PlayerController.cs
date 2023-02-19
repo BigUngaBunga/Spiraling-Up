5fb362f75d1220651aaaba6799629407d5d576b1
Tpbezerra <81422026+Tpbezerra@users.noreply.github.com>
2023-02-19 15:01:20 +0100

    Jump buffer, coyote time, grounded change
    Added a buffer when pressing the jump key. Added coyote time. Changed the way being grounded is calculated as it would previously think the player was grounded for a moment before physics calculations had been run.

--- Visually Difficult/Assets/Scripts/PlayerController.cs
+++ Visually Difficult/Assets/Scripts/PlayerController.cs
@@ -6,38 +6,51 @@ using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
-    [Header("Movement")]
-    [SerializeField] private float speed;
+    [Header("Setup")]
     [SerializeField] private LayerMask groundMask;
     [SerializeField] private float rayLength;
-    [Range(0, 0.33f)]
-    [SerializeField] private float drag;
-    [SerializeField] private float acceleration;
-    private GameObject topRayOrigin, bottomRayOrigin;
-    [SerializeField]private Vector2 lastDirection = Vector2.zero;
+
+    [Header("Movement")]
+    [SerializeField] private float speed = 10;
+    [SerializeField] private float deceleration = 20;
+    [SerializeField] private float acceleration = 15;
 
     [Header("Jumping")]
-    [SerializeField] private float jumpForce;
-    [Range(0, 1f)]
-    [SerializeField] private float jumpDecay;
-    [SerializeField] private float wallJumpForce;
-    [Range(0, 90f)]
-    [SerializeField] private float wallJumpAngle;
+    [SerializeField] private float jumpVelocity = 13;
+    [SerializeField] private float jumpDecay = 26;
+    [SerializeField] private float jumpBuffer = 0.2f;
+    [SerializeField] private float coyoteTime = 0.1f;
 
+    [Header("Wall Jumping")]
+    [SerializeField] private float wallJumpVelocity = 15;
+    [Range(0, 90f)]
+    [SerializeField] private float wallJumpAngle = 45;
+    [SerializeField] private float wallJumpDisableDuration = 0.2f;
 
     [Header("Ground check")]
     [SerializeField] private Vector2 groundCheckOffset;
     [SerializeField] private Vector2 groundCheckSize;
-    private Vector2 GroundCheckPosition => (Vector2)transform.position + groundCheckOffset;
 
     [Header("Debug")]
     [SerializeField] private bool drawDebug;
     [SerializeField] private bool printDebug;
     [SerializeField] private bool spaceForWallJump;
 
+    private GameObject topRayOrigin, bottomRayOrigin;
+
+    private Vector2 lastDirection;
+
+    private bool isGrounded;
+    private bool inputDisabled;
+
+    private float jumpTimer;
+    private float coyoteTimer;
+
     private PlayerInput input;
     private new Rigidbody2D rigidbody;
-    
+
+    private Vector2 GroundCheckPosition => (Vector2)transform.position + groundCheckOffset;
+
     #region Actions
     private InputAction moveAction;
     private InputAction jumpAction;
@@ -61,8 +74,8 @@ public class PlayerController : MonoBehaviour
         rigidbody = GetComponent<Rigidbody2D>();
         input = GetComponent<PlayerInput>();
 
-        topRayOrigin = GameObject.Find("TopRay");
-        bottomRayOrigin = GameObject.Find("BottomRay");
+        topRayOrigin = transform.Find("TopRay").gameObject;
+        bottomRayOrigin = transform.Find("BottomRay").gameObject;
 
         moveAction = input.actions["Move"];
         jumpAction = input.actions["Jump"];
@@ -77,46 +90,72 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         AddMovement(moveAction.ReadValue<Vector2>());
+        Jumping();
+
+        coyoteTimer = isGrounded ? 0 : coyoteTimer + Time.fixedDeltaTime;
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (isGrounded || groundMask != (groundMask | (1 << collision.gameObject.layer)))
+            return;
+
+        for (int i = 0; i < collision.contactCount; i++)
+        {
+            if (Vector2.Angle(collision.GetContact(i).normal, Vector2.up) < 45)
+            {
+                isGrounded = true;
+                return;
+            }
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isGrounded = false;
     }
 
     private void Jump(InputAction.CallbackContext context)
     {
-        if (spaceForWallJump)
-        {
-            if (WallInDirection(Vector2.left, true))
-                WallJump(Vector2.right);
-            else if (WallInDirection(Vector2.right, true))
-                WallJump(Vector2.left);
-        }
-        if (IsGrounded())
-        {
-            SetY();
-            rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            StartCoroutine(ContinueJump());
-        }
+        jumpTimer = jumpBuffer;
     }
 
     private void WallJump(Vector2 jumpDirection)
     {
-        float angle = wallJumpAngle / Mathf.Rad2Deg;
-        Vector2 force = new Vector2(jumpDirection.x * Mathf.Cos(angle), Mathf.Sin(angle)) * wallJumpForce;
-        SetY();
-        rigidbody.AddForce(force, ForceMode2D.Impulse);
+        float angle = wallJumpAngle * Mathf.Deg2Rad;
+        Vector2 velocity = new Vector2(jumpDirection.x * Mathf.Sin(angle), Mathf.Cos(angle)) * wallJumpVelocity;
+        rigidbody.velocity = velocity;
+
+        jumpTimer = -1;
+        coyoteTimer = coyoteTime;
+
+        inputDisabled = true;
+        StopAllCoroutines();
+        StartCoroutine(ReenableInput());
     }
 
     private IEnumerator ContinueJump()
     {
-        while (jumpAction.IsPressed())
+        WaitForFixedUpdate wait = new ();
+
+        while (jumpAction.IsPressed() && rigidbody.velocity.y > 0)
         {
             Print("ContinuedJump");
-            yield return new WaitForFixedUpdate();
+            yield return wait;
         }
         Print("Stopped Jump");
-        while (rigidbody.velocity.y > 0.1f)
+        while (rigidbody.velocity.y > 0)
         {
-            LerpY(0, jumpDecay);
-            yield return new WaitForFixedUpdate();
+            LerpY(0, jumpDecay * Time.fixedDeltaTime);
+            yield return wait;
         }
+    }
+
+    private IEnumerator ReenableInput()
+    {
+        yield return new WaitForSeconds(wallJumpDisableDuration);
+
+        inputDisabled = false;
     }
 
     private void AddMovement(Vector2 direction)
@@ -124,15 +163,44 @@ public class PlayerController : MonoBehaviour
         if (!spaceForWallJump)
             EvaluateWallJump(direction);
             
-        if (!IsGrounded() && WallInDirection(direction)) 
+        if (inputDisabled || (!isGrounded && WallInDirection(direction))) 
             return;
-        if (direction == Vector2.zero){
-            LerpX(0, drag); return;}
-        //TODO kika på om man borde reducera kontroll i luften
-        Print("Is Moving");
-        float targetSpeed = direction.x * speed;
-        LerpX(targetSpeed, acceleration * Time.fixedDeltaTime);
-        lastDirection = direction;
+
+        if (direction == Vector2.zero || (rigidbody.velocity.x > 0 && direction.x < 0) || (rigidbody.velocity.x < 0 && direction.x > 0))
+            LerpX(0, deceleration * Time.fixedDeltaTime);
+
+        if (direction != Vector2.zero)
+        {
+            //TODO kika på om man borde reducera kontroll i luften
+            Print("Is Moving");
+            float targetSpeed = direction.x * speed;
+            LerpX(targetSpeed, acceleration * Time.fixedDeltaTime);
+            lastDirection = direction;
+        }
+    }
+
+    private void Jumping()
+    {
+        if (jumpTimer <= 0)
+            return;
+
+        if (coyoteTimer < coyoteTime)
+        {
+            SetY(jumpVelocity);
+            StartCoroutine(ContinueJump());
+
+            jumpTimer = -1;
+            coyoteTimer = coyoteTime;
+        }
+        else if (spaceForWallJump)
+        {
+            if (WallInDirection(Vector2.left, true))
+                WallJump(Vector2.right);
+            else if (WallInDirection(Vector2.right, true))
+                WallJump(Vector2.left);
+        }
+
+        jumpTimer -= Time.fixedDeltaTime;
     }
 
     private void EvaluateWallJump(Vector2 direction)
@@ -143,20 +211,22 @@ public class PlayerController : MonoBehaviour
 
     #region Velocity
     private void SetX(float speed = 0) => rigidbody.velocity = new Vector2(speed, rigidbody.velocity.y);
-    private void LerpX(float targetSpeed, float lerpRate) => SetX(Mathf.Lerp(rigidbody.velocity.x, targetSpeed, lerpRate));
+    private void LerpX(float targetSpeed, float lerpRate) => SetX(Mathf.MoveTowards(rigidbody.velocity.x, targetSpeed, lerpRate));
     private void SetY(float speed = 0) => rigidbody.velocity = new Vector2(rigidbody.velocity.x, speed);
-    private void LerpY(float targetSpeed, float lerpRate) => SetY(Mathf.Lerp(rigidbody.velocity.y, targetSpeed, lerpRate));
+    private void LerpY(float targetSpeed, float lerpRate) => SetY(Mathf.MoveTowards(rigidbody.velocity.y, targetSpeed, lerpRate));
     #endregion
 
     #region Ground Check
-    private bool IsGrounded() => Physics2D.OverlapBox(GroundCheckPosition, groundCheckSize, 0f, groundMask);
+    private bool IsGrounded() => Physics2D.OverlapBox(GroundCheckPosition, groundCheckSize, 0, groundMask);
     private bool RayHit(RaycastHit2D raycast) => raycast.transform != null;
     private bool WallInDirection(Vector2 direction, bool dontTouchGround = false)
     {
         var topRay = Physics2D.Raycast(topRayOrigin.transform.position, direction, rayLength, groundMask);
         var bottomRay = Physics2D.Raycast(bottomRayOrigin.transform.position, direction, rayLength, groundMask);
+
         bool hitTop = RayHit(topRay);
-        bool hitBottom = dontTouchGround ? (RayHit(bottomRay) && !IsGrounded()) : RayHit(bottomRay);
+        bool hitBottom = (dontTouchGround ? (RayHit(bottomRay) && !IsGrounded()) : RayHit(bottomRay));
+        
         return hitTop || hitBottom;
     }
     #endregion
@@ -184,6 +254,5 @@ public class PlayerController : MonoBehaviour
         if (printDebug)
             Debug.Log(text);
     }
-
     #endregion
 }
