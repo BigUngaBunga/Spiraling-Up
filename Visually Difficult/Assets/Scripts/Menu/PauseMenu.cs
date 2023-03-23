d54966e2d8b51ff6205678fb82c86982e031343b
BigUngaBunga <truls.rockstrom.ystad.nu@gmail.com>
2023-03-23 12:03:03 +0100

    Added pause menu and controller support
    Added a pause menu which sets time scale to 0 and has buttons to continue, go to main menu and exit the game.

---
+++ Visually Difficult/Assets/Scripts/Menu/PauseMenu.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject menu;
+    private bool isPaused = false;
+    private InputAction pauseAction;
+
+    private void Awake()
+    {
+        pauseAction = GetComponent<PlayerInput>().actions["Menu"];
+        menu.SetActive(isPaused);
+    }
+
+    private void OnDisable() => pauseAction.performed -= OnPause;
+    private void OnEnable() => pauseAction.performed += OnPause;
+
+    private void OnPause(InputAction.CallbackContext context) => TogglePauseMenu();
+
+    public void TogglePauseMenu()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = isPaused ? 0 : 1;
+        menu.SetActive(isPaused);
+
+        Cursor.visible = isPaused;
+        Cursor.lockState = isPaused ? CursorLockMode.Confined : CursorLockMode.Locked;
+    }
+
+    public void LoadMainMenu()
+    {
+        var dontDestroyObjects = FindObjectsByType(typeof(DontDestroy), FindObjectsInactive.Include, FindObjectsSortMode.None);
+        for (int i = dontDestroyObjects.Length -1; i >= 0; --i)
+            Destroy(dontDestroyObjects[i].GameObject());
+        SceneManager.LoadSceneAsync(0);
+    }
+
+    public void ExitGame() => Application.Quit();
+
+}
