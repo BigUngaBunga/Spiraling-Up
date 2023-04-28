3b77b724d6170d7db4922edbf55e1ead798ddf46
Tpbezerra <81422026+Tpbezerra@users.noreply.github.com>
2023-04-28 17:51:54 +0200

    Parallax pipes fix

--- Visually Difficult/Assets/Scripts/Map objects/ParallaxElement.cs
+++ Visually Difficult/Assets/Scripts/Map objects/ParallaxElement.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class ParallaxElement : MonoBehaviour
@@ -12,14 +14,19 @@ public class ParallaxElement : MonoBehaviour
 
     protected Vector3 moveX, moveY;
 
-    protected virtual void Start()
+    void Start()
     {
         initialPosition = transform.position;
         if (!overrideBounds)
+        {
+            quaternion rotation = transform.rotation;
+            transform.rotation = quaternion.identity;
             bounds = GetComponent<SpriteRenderer>().bounds.size;
+            transform.rotation = rotation;
+        }
 
-        moveX = new Vector3(bounds.x * 2 - 1, 0);
-        moveY = new Vector3(0, bounds.y * 2 - 1);
+        moveX = new Vector3(bounds.x * 2, 0);
+        moveY = new Vector3(0, bounds.y * 2);
 
         Camera.main.gameObject.GetComponent<ParallaxScroller>().AddElement(this);
     }
@@ -30,6 +37,13 @@ public class ParallaxElement : MonoBehaviour
             transform.position = initialPosition + new Vector3(distance.x * moveScale.x, distance.y * moveScale.y);
         else
             transform.position = new Vector3(initialPosition.x + distance.x * moveScale.x, transform.position.y);
+
+        StartCoroutine(MoveInitialPosition(distance));
+    }
+
+    IEnumerator MoveInitialPosition(Vector3 distance)
+    {
+        yield return new WaitForEndOfFrame();
 
         if (Position.x + bounds.x < distance.x)
             initialPosition += moveX;
